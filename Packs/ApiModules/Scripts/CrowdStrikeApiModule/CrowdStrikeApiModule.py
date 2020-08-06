bc2737f6b2a6be0ca1c5d818ac85685a592b5831
guyfreund <gfreund@paloaltonetworks.com>
2020-08-06 16:21:56 +0300

    cs actors

--- Packs/ApiModules/Scripts/CrowdStrikeApiModule/CrowdStrikeApiModule.py
+++ Packs/ApiModules/Scripts/CrowdStrikeApiModule/CrowdStrikeApiModule.py
@@ -27,12 +27,12 @@ class CrowdStrikeClient(BaseClient):
         :param res: the request's response
         :return: None
         """
-        err_msg = 'Error in API call [{}] - {}'.format(res.status_code, res.reason)
+        err_msg = 'Error in API call [{}] - {}\n'.format(res.status_code, res.reason)
         try:
             # Try to parse json error response
             error_entry = res.json()
             errors = error_entry.get("errors", [])
-            err_msg += '\n' + '\n'.join(f"{error['code']}: {error['message']}" for error in errors)
+            err_msg += '\n'.join(f"{error['code']}: {error['message']}" for error in errors)
             raise DemistoException(err_msg)
         except ValueError:
             err_msg += '\n{}'.format(res.text)
--- Packs/CrowdStrikeIntel/Integrations/CrowdStrikeFlaconIntel/CrowdStrikeFalconIntel.py
+++ Packs/CrowdStrikeIntel/Integrations/CrowdStrikeFlaconIntel/CrowdStrikeFalconIntel.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple, List
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
+from datetime import datetime
 
 import urllib3
 import traceback
@@ -16,11 +17,46 @@ class Client:
     bla bla
     """
     def __init__(self, params):
-        self.cs_client = CrowdStrikeClient(params=params)
-        self.threshold = params.get('threshold')
+        self._cs_client = CrowdStrikeClient(params=params)
+        self._threshold = params.get('threshold')
+        self.query_params = ['offset', 'limit', 'sort', 'q', 'include_deleted']
+        self.date_params = {
+            'created_date': {'operator': '', 'raw_name': 'created_date'},
+            'max_last_modified_date': {'operator': '<=', 'api_key': 'last_modified_date'},
+            'min_last_activity_date': {'operator': '>=', 'api_key': 'first_activity_date'},
+            'max_last_activity_date': {'operator': '<=', 'api_key': 'last_activity_date'}
+        }
+
+    def build_params(self, args: dict) -> dict:
+        params = {key: args.get(key) for key in self.query_params}
+        params['filter'] = self.build_filter_query(args)
+        return assign_params(**params)
+
+    @staticmethod
+    def date_to_epoch(date: str) -> int:
+        return int(datetime.fromisoformat(date).timestamp())
+
+    def build_filter_query(self, args: dict) -> str:
+        filter_query = str()
+
+        for key in args:
+            if key not in self.query_params:
+                if key not in self.date_params:
+                    values = argToList(args[key], ',')
+                    for value in values:
+                        filter_query += f"{key}:'{value}'+"
+                else:
+                    operator = self.date_params.get(key, {}).get('operator')
+                    api_key = self.date_params.get(key, {}).get('api_key')
+                    filter_query += f"{api_key}:{operator}{self.date_to_epoch(args[key])}+"
+
+        if filter_query.endswith('+'):
+            filter_query = filter_query[:-1]
+
+        return filter_query
 
     def check_quota_status(self):
-        return self.cs_client.check_quota_status()
+        return self._cs_client.check_quota_status()
 
     def file(self):
         pass
@@ -34,11 +70,11 @@ class Client:
     def domain(self):
         pass
 
-    def cs_actors(self):
-        pass
+    def cs_actors(self, args):
+        return self._cs_client.http_request('GET', 'intel/combined/actors/v1', params=self.build_params(args))
 
-    def cs_indicators(self):
-        pass
+    def cs_indicators(self, args):
+        return self._cs_client.http_request('GET', 'intel/combined/indicators/v1', params=self.build_params(args))
 
     def cs_reports(self):
         pass
@@ -49,9 +85,10 @@ class Client:
 
 def test_module(client: Client):
     """
-    If a client was made then an accesses token was successfully reached,
-    therefor the username and password are valid and a connection was made
-    additionally, checks if not using all the optional quota
+    If a client is successfully constructed then an accesses token was successfully reached,
+    therefore the username and password are valid and a connection was made.
+    Additionally, checks if not using all the optional quota and check that an http request to actors & indicators
+    endpoints in successful.
     :param client: the client object with an access token
     :return: ok if got a valid accesses token and not all the quota is used at the moment
     """
@@ -70,8 +107,8 @@ def test_module(client: Client):
             if total <= used:
                 raise Exception(f'Quota limitation has been reached: {used}')
             else:
-                client.cs_client.http_request('GET', f'intel/combined/indicators/v1', params={'limit': 1})
-                client.cs_client.http_request('GET', f'intel/combined/actors/v1', params={'limit': 1})
+                client._cs_client.http_request('GET', 'intel/combined/indicators/v1', params={'limit': 1})
+                client._cs_client.http_request('GET', 'intel/combined/actors/v1', params={'limit': 1})
                 return 'ok'
     raise Exception('Quota limitation is unreachable')
 
@@ -93,7 +130,74 @@ def domain_command(client: Client):
 
 
 def cs_actors_command(client: Client):
-    pass
+    res = client.cs_actors(demisto.args())
+    md = '## Falcon Intel Actor search\n'
+    resources = res.get('resources', {})
+    if resources:
+        for r in resources:
+            image = r.get('image', {})
+            image_url = image.get('url')
+            name = r.get('name')
+            actor_id = r.get('id')
+            url = r.get('url')
+            slug = r.get('slug')
+            short_description = r.get('short_description')
+            first_activity_date = r.get('first_activity_date')
+            last_activity_date = r.get('last_activity_date')
+            active = r.get('active')
+            known_as = r.get('known_as')
+            target_industries = r.get('target_industries')
+            target_countries = r.get('target_countries')
+            origins = r.get('origins')
+            motivations = r.get('motivations')
+            capability = r.get('capability', {})
+            capability_value = capability.get('value')
+            group = r.get('group')
+            region = r.get('region', {})
+            region_value = region.get('value')
+            kill_chain = r.get('kill_chain')
+
+            if image_url:
+                md += '![' + name + '](' + image_url + ' "' + name + '")\n'
+            md += '### ' + name + '\n'
+            md += '- ID: [' + str(actor_id) + '](' + url + ')\n'
+            md += '- Slug: ' + slug + '\n'
+            md += '- Short description: ' + short_description + '\n'
+            md += '- First/Last activity: ' + timestamp_to_datestring(first_activity_date) + ' / ' + \
+                  timestamp_to_datestring(last_activity_date) + '\n'
+            md += '- Active: ' + str(active) if active is not None else '- Active: Unknown'
+            md += '\n'
+            md += '- Known as: ' + known_as if known_as else 'Unknown'
+            md += '\n'
+            md += '-  Target industries: ' + take_value(target_industries) if target_industries else ''
+            md += '\n'
+            md += '- Target countries: ' + take_value(target_countries) if target_countries else ''
+            md += '\n'
+            md += '- Origins: ' + take_value(origins) if origins else ''
+            md += '\n'
+            md += '- Motivations: ' + take_value(motivations) if motivations else ''
+            md += '\n'
+            md += '- Capability: ' + capability_value if capability_value else '- Capability: Unknown'
+            md += '\n'
+            md += '- Group: ' + group if group else '- Group: Unknown'
+            md += '\n'
+            md += '- Region: ' + region_value if region_value else '- Region: Unknown'
+            md += '\n'
+            if kill_chain:
+                md += '#### Kill chain\n'
+                for kc_field in kill_chain:
+                    if 'rich_text' in kc_field and kc_field.index('rich_text') == 0:
+                        continue
+                    md += '- ' + string_to_table_header(kc_field) + ': ' + kill_chain.get(kc_field)
+                md += '\n'
+    else:
+        md = 'No actors found.'
+    return md, {}, res
+
+
+def take_value(l: list) -> str:
+    nl = [i.get('value') for i in l]
+    return ', '.join(nl)
 
 
 def cs_indicators_command(client: Client):
@@ -115,23 +219,26 @@ def main():
         LOG(f'Command being called in CrowdStrikeFalconX Sandbox is: {command}')
         client = Client(params=params)
         if command == 'test-module':
-            return_results(test_module(client))
+            return_outputs(test_module(client))
         elif command == 'file':
-            return_results(file_command(client))
+            return_outputs(file_command(client))
         elif command == 'ip':
-            return_results(ip_command(client))
+            return_outputs(ip_command(client))
         elif command == 'url':
-            return_results(url_command(client))
+            return_outputs(url_command(client))
         elif command == 'domain':
-            return_results(domain_command(client))
+            return_outputs(domain_command(client))
         elif command == 'cs-actors':
-            return_results(cs_actors_command(client))
+            hr, ops, raw = cs_actors_command(client)
+            return_outputs(hr, ops, raw)
         elif command == 'cs-indicators':
-            return_results(cs_indicators_command(client))
+            hr, ops, raw = cs_indicators_command(client)
+            return_outputs(hr, ops, raw)
         elif command == 'cs-reports':
-            return_results(cs_reports_command(client))
+            hr, ops, raw = cs_reports_command(client)
+            return_outputs(hr, ops, raw)
         elif command == 'cs-report-pdf':
-            return_results(cs_report_pdf_command(client))  # Might not be needed
+            return_outputs(cs_report_pdf_command(client))
         else:
             raise NotImplementedError(f'{command} is not an existing CrowdStrike Falcon Intel command')
     except Exception as err:
